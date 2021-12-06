3b37fd68dc392bc1b42dc9c225186fdb69fa139b
Ilias <igoujgali@gmail.com>
2021-12-06 09:50:50 +0100

    Change status code for sign up

--- src/main/java/amt/auth/Controller/AuthController.java
+++ src/main/java/amt/auth/Controller/AuthController.java
@@ -1,9 +1,10 @@
 package amt.auth.Controller;
 
 import amt.auth.DTO.AccountDTO;
+import amt.auth.DTO.CredentialDTO;
 import amt.auth.DTO.TokenDTO;
 import amt.auth.Service.AuthService;
-import amt.auth.DTO.CredentialDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,17 +21,19 @@ public class AuthController {
 
     /**
      * Endpoint de création de compte
+     *
      * @param credentialDTO crédential du nouvel utilisateur
      * @return ResponseEntity<AccountDTO>
      */
     @PostMapping(value = "/account/register")
     public ResponseEntity<AccountDTO> signup(@Validated @RequestBody CredentialDTO credentialDTO) {
 
-        return ResponseEntity.ok(authService.signup(credentialDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(authService.signup(credentialDTO));
     }
 
     /**
      * Endpoint de connexion à un compte existant
+     *
      * @param credentialDTO credential de l'utilisateur
      * @return ResponseEntity<TokenDTO>
      */
